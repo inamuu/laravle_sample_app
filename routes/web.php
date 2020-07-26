4051d94b3760f128555ee75d78b93fa500dd1aa7
inamuu <8310973+inamuu@users.noreply.github.com>
2020-07-27 00:05:32 +0900

    :bug: Fix home 404

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+//Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/', 'HomeController@index')->name('home');
