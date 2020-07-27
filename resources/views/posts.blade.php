a08141b525a73e201323d9f6cae49685604e2e9f
inamuu <8310973+inamuu@users.noreply.github.com>
2020-07-27 18:33:26 +0900

    :+1: fix display all posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,9 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
     <div class="col-md-8">
+    @foreach ($posts as $post)
         <div class="card">
-        <!-- <div class="card-header">{{ __('Dashboard') }}</div> -->
-
         <div class="card-body">
             @if (session('status'))
             <div class="alert alert-success" role="alert">
@@ -14,33 +13,26 @@
             </div>
             @endif
 
-
-            <!-- Books -->
             @if (count($posts) > 0)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        記事一覧
-                    </div>
-
-                    <div class="panel-body">
-                        <table class="table table-striped task-table">
-                            <tbody>
-                                @foreach ($posts as $post)
-                                    <tr>
-                                        <td class="table-text"><div>タイトル: {{ $post->title }}</div></td>
-                                        <td class="table-text"><div>投稿日時: {{ $post->created_at }}</div></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="table-text"><p>{{ $post->content }}</p></td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <table class="table table-striped task-table">
+                        <tbody>
+                            <tr>
+                                <td class="table-text"><div>タイトル: {{ $post->title }}</div></td>
+                                <td class="table-text"><div>投稿日時: {{ $post->created_at }}</div></td>
+                            </tr>
+                            <tr>
+                                <td class="table-text"><div>{{ $post->content }}</div></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
+            </div>
             @endif
         </div>
         </div>
+    @endforeach
     </div>
     </div>
 </div>
